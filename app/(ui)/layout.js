87412fdafc04582b950e5d47e60bbe5ad24e6940
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2024-10-30 14:45:01 +0700

    update

--- app/(ui)/layout.js
+++ app/(ui)/layout.js
@@ -1,3 +1,115 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  DehazeOutlined,
+  AutoGraphOutlined,
+  CampaignOutlined,
+  DocumentScannerOutlined,
+  SlowMotionVideoOutlined,
+  CottageOutlined,
+  SettingsSuggestOutlined,
+  FilterDramaOutlined,
+  CalendarMonthOutlined,
+  RingVolumeOutlined,
+  ExtensionOutlined,
+  PersonOutlineOutlined,
+  ComputerOutlined,
+  Face5Outlined,
+  ExitToAppOutlined,
+} from "@mui/icons-material";
+
+function MenuCard({ href, icons, onClick }) {
+  return (
+    <Link
+      href={href}
+      className="flex items-center justify-center w-14 h-14 p-2 hover:text-[#635bff] hover:bg-[#635bff]/25 rounded-xl border-2 border-[#000000] border-dashed"
+    >
+      {icons}
+    </Link>
+  );
+}
+
 export default function UiLayout({ children }) {
-  return <div className="flex flex-row items-center justify-center w-full h-full p-2 gap-2 border-2 border-[#000000] border-dashed">{children}</div>;
+  return (
+    <div className="flex flex-row items-center justify-center w-full min-h-screen p-2 gap-2 border-2 border-[#000000] border-dashed">
+      <div className="flex flex-row items-center justify-center w-3/12 min-h-screen p-2 gap-2 border-2 border-[#000000] border-dashed">
+        <div className="flex flex-col items-center justify-start w-3/12 min-h-screen p-2 border-2 border-[#000000] border-dashed bg-[#F3F7FB] overflow-auto">
+          <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-b-2">
+            <button className="flex items-center justify-center w-14 h-14 p-2 border-2 border-[#000000] border-dashed rounded-xl">
+              <DehazeOutlined />
+            </button>
+            <MenuCard
+              href="/"
+              icons={<AutoGraphOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+            <MenuCard
+              href="/"
+              icons={<CampaignOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+            <MenuCard
+              href="/"
+              icons={<DocumentScannerOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+            <MenuCard
+              href="/"
+              icons={<SlowMotionVideoOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-b-2">
+            <MenuCard
+              href="/"
+              icons={<CottageOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+            <MenuCard
+              href="/hr"
+              icons={<PersonOutlineOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+            <MenuCard
+              href="/it"
+              icons={<ComputerOutlined style={{ fontSize: "1.5rem" }} />}
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-b-2">
+            <MenuCard
+              href="/"
+              icons={<Face5Outlined style={{ fontSize: "2rem" }} />}
+            />
+            <button
+              href="/#"
+              className="flex items-center justify-center w-14 h-14 p-2 text-[#FFFFFF] bg-[#635bff] rounded-xl"
+            >
+              <ExitToAppOutlined />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-start w-9/12 min-h-screen p-2 gap-2 border-2 border-[#000000] border-dashed bg-[#FFFFFF] overflow-auto">
+          <div className="flex flex-row items-center justify-start w-full h-16 p-2 gap-2 border-2 border-[#000000] border-dashed rounded-xl">
+            <Image
+              src="/images/other/company_logo.png"
+              alt="company_logo"
+              width={50}
+              height={50}
+              priority={true}
+            />
+            Channakorn - Cis
+          </div>
+          <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-[#000000] border-dashed rounded-xl">
+            <div className="flex items-center justify-start w-full h-full p-2 border-2 border-[#000000] border-dashed rounded-xl">
+              สาขา
+            </div>
+            <div className="flex items-center justify-start w-full h-full p-2 border-2 border-[#000000] border-dashed rounded-xl">
+              ฝ่าย
+            </div>
+            <div className="flex items-center justify-start w-full h-full p-2 border-2 border-[#000000] border-dashed rounded-xl">
+              แผนก
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center w-9/12 min-h-screen p-2 gap-2 border-2 border-[#000000] border-dashed">
+        01
+      </div>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,123 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { toast, Toaster } from "react-hot-toast";
+import React, { useRef, useEffect, useState } from "react";
+import { Input, Button } from "@nextui-org/react";
+import { useSession, signIn } from "next-auth/react";
 
 export default function page() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const { data: session } = useSession();
+  const router = useRouter();
+  const useridcardRef = useRef(null);
+  const userPasswordRef = useRef(null);
+
+  useEffect(() => {
+    if (session) {
+      router.push("/home");
+    }
+  }, [session, router]);
+
+  const handlerLogin = async (event) => {
+    event.preventDefault();
+
+    const useridcard = useridcardRef.current?.value?.trim();
+    const userPassword = userPasswordRef.current?.value?.trim();
+    const secretToken = process.env.NEXT_PUBLIC_SECRET_TOKEN;
+
+    if (!useridcard || !userPassword) {
+      toast.error("Please Enter Your User Id card And Password");
+      return;
+    }
+
+    try {
+      const result = await signIn("credentials", {
+        redirect: false,
+        user_id_card: useridcard,
+        user_password: userPassword,
+        secret_token: secretToken,
+      });
+
+      if (result.error) {
+        toast.error("Login Failed");
+      } else {
+        toast.success("Login Successful! Welcome To Cis System");
+        setTimeout(() => {
+          router.push("/home");
+        }, 2000);
+      }
+    } catch (error) {
+      toast.error("An Unexpected Error Occurred. Please Try Again Later");
+      console.error("Login error:", error);
+    }
+  };
+
+  if (!session) {
+    return (
+      <div className="flex items-center justify-center w-full min-h-screen p-2">
+        <Toaster position="top-right" reverseOrder={false} />
+        <form className="flex flex-col items-center justify-center w-full h-full lg:w-4/12 lg:h-4/12 p-2 gap-2 border-2 bg-[#FFFFFF] shadow-md rounded-3xl">
+          <div className="flex items-center justify-center w-full h-full p-2">
+            <Image
+              src="/images/other/company_logo.png"
+              alt="company_logo"
+              width={100}
+              height={100}
+              priority={true}
+            />
+          </div>
+          <div className="flex items-center justify-center w-full h-full p-2 text-[#000000] text-xl font-[600]">
+            Channakorn Engineering
+          </div>
+          <div className="flex items-center justify-center w-full h-full p-2 text-[#000000] text-xl font-[600]">
+            Internal System
+          </div>
+          <div className="flex flex-col items-center justify-center w-full h-full p-2">
+            <div className="flex items-center justify-start w-full h-full p-2 text-[#000000] text-ทก font-[300]">
+              เลขบัตรประชาชน
+            </div>
+            <div className="flex items-center justify-center w-full h-full p-2">
+              <Input
+                type="text"
+                placeholder="กรุณาระบุข้อมูล"
+                size="lg"
+                variant="bordered"
+                ref={useridcardRef}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-full h-full p-2">
+            <div className="flex items-center justify-start w-full h-full p-2 text-[#000000] text-ทก font-[300]">
+              รหัสผ่าน
+            </div>
+            <div className="flex items-center justify-center w-full h-full p-2">
+              <Input
+                type="password"
+                placeholder="กรุณาระบุข้อมูล"
+                size="lg"
+                variant="bordered"
+                ref={userPasswordRef}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-center w-full h-full p-2">
+            <Button
+              onClick={handlerLogin}
+              size="lg"
+              className="w-1/2 bg-[#16cdc7] text-[#FFFFFF]"
+            >
+              เข้าสู่ระบบ
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
